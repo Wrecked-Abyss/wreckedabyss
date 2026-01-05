ca5853e27852f9c0143bc28bb927acd06ceb1525
Wrecked-Abyss <wreckedabyss@gmail.com>
2026-01-06 00:22:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  */
 (function() {
     emailjs.init({
-        publicKey: "mj4_klMqUt_du4lXS", // Hard-code your actual key here
+        publicKey: "mj4_klMqOt_du4lXS", // Hard-code your actual key here
     });
 })();
 
@@ -130,6 +130,7 @@ if (contactForm) {
             });
     });
 }
+
 
 
 
